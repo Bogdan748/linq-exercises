11cbe800f4c9830080dc9f462eeb2e1fa3743852
Bogdan748 <andriesbogdan00@gmail.com>
2022-03-28 21:40:42 +0300

    Linq - sintax Varianta 1 & Varianta 2

---
+++ LinqExercises/LinqExercises/NumbersGenerator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqExercises
+{
+    public class NumbersGenerator
+    {
+        public static IEnumerable<long> Generate()
+        {
+            long start = 0;
+            while (true)
+            {
+                //Console.WriteLine($"Infinite generator: {start}");
+                yield return start;
+
+                if(start < int.MaxValue)
+                {
+                    start++;
+                }
+                else
+                {
+                    start = 0;
+                }                
+            }
+        }
+    }
+}
